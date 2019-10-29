bad9e1d94ae9acba3fe7c1e65de1b21a211943d6
liuyao <reactjser@163.com>
2019-10-29 15:38:59 +0800

    redux integration

---
+++ src/store/models.js
@@ -0,0 +1,32 @@
+export const counter = {
+  state: {
+    value: 0
+  },
+  reducers: {
+    // handle state changes with pure functions
+    increment(state, payload) {
+      return {
+        ...state,
+        value: state.value + 1
+      };
+    },
+    decrement(state, payload) {
+      return {
+        ...state,
+        value: state.value - 1
+      };
+    }
+  },
+  effects: {
+    // handle state changes with impure functions.
+    // use async/await for async actions
+    async incrementAsync(payload, rootState) {
+      await new Promise(resolve => setTimeout(resolve, 1000));
+      this.increment(payload);
+    },
+    async decrementAsync(payload, rootState) {
+      await new Promise(resolve => setTimeout(resolve, 1000));
+      this.decrement(payload);
+    }
+  }
+};
--- src/views/About.js
+++ src/views/About.js
@@ -1,11 +1,39 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const About = () => {
+const About = ({
+  count,
+  increment,
+  incrementAsync,
+  decrement,
+  decrementAsync
+}) => {
   return (
     <div className="about">
       <h1>This is an about page</h1>
+      <button onClick={increment}>同步+1</button>{' '}
+      <button onClick={incrementAsync}>异步+1</button>{' '}
+      <button onClick={decrement}>同步-1</button>{' '}
+      <button onClick={decrementAsync}>异步-1</button>{' '}
+      <input type="text" value={count} readOnly />
     </div>
   );
 };
 
-export default About;
+const mapState = state => ({
+  count: state.counter.value
+});
+
+const mapDispatch = ({
+  counter: { increment, incrementAsync, decrement, decrementAsync }
+}) => ({
+  increment: () => increment(1),
+  incrementAsync: () => incrementAsync(1),
+  decrement: () => decrement(1),
+  decrementAsync: () => decrementAsync(1)
+});
+
+export default connect(
+  mapState,
+  mapDispatch
+)(About);
