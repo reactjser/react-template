51ca0a94050f82020b253a3d17d641181ebf34e9
liuyao <reactjser@163.com>
2019-10-31 14:12:09 +0800

    immer

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,14 +1,36 @@
 import React from 'react';
+import produce from 'immer';
 import Logo from '../assets/logo.png';
-import HelloWorld from '../components/HelloWorld'
+import HelloWorld from '../components/HelloWorld';
 
 const Home = () => {
+  const [members, setMembers] = React.useState([
+    {
+      name: 'ronffy',
+      age: 30
+    }
+  ]);
+
+  React.useEffect(() => {
+    console.log(members);
+  }, [members]);
+
   return (
     <div className="home">
-      <img alt="Vue logo" src={Logo} />
+      <img
+        alt="Vue logo"
+        src={Logo}
+        onClick={() => {
+          setMembers(
+            produce(members, draft => {
+              draft[0].age++;
+            })
+          );
+        }}
+      />
       <HelloWorld msg="Welcome to Your Vue.js App" />
     </div>
   );
-}
+};
 
 export default Home;
