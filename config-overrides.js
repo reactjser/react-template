cc49cd3cdd3ba21cb5ea6232c4877eb7cf5ec765
liuyao <reactjser@163.com>
2019-11-01 12:13:18 +0800

    add alias

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,13 @@
-const { override, fixBabelImports } = require('customize-cra');
+const path = require('path');
+const { override, fixBabelImports, addWebpackAlias } = require('customize-cra');
 
 module.exports = override(
   fixBabelImports('import', {
     libraryName: 'antd',
     libraryDirectory: 'es',
-    style: 'css',
+    style: 'css'
   }),
+  addWebpackAlias({
+    '@': path.join(__dirname, 'src')
+  })
 );
