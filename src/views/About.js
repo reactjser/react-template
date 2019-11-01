0836e653bb15693a7a0badb8c8673e3ed72d63b9
liuyao <reactjser@163.com>
2019-11-01 09:57:46 +0800

    antd

--- src/views/About.js
+++ src/views/About.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Button, InputNumber } from 'antd';
 
 const About = ({
   count,
@@ -11,11 +12,11 @@ const About = ({
   return (
     <div className="about">
       <h1>This is an about page</h1>
-      <button onClick={increment}>同步+1</button>{' '}
-      <button onClick={incrementAsync}>异步+1</button>{' '}
-      <button onClick={decrement}>同步-1</button>{' '}
-      <button onClick={decrementAsync}>异步-1</button>{' '}
-      <input type="text" value={count} readOnly />
+      <Button type="primary" onClick={increment}>同步+1</Button>{' '}
+      <Button onClick={incrementAsync}>异步+1</Button>{' '}
+      <Button onClick={decrement}>同步-1</Button>{' '}
+      <Button onClick={decrementAsync}>异步-1</Button>{' '}
+      <InputNumber type="text" value={count} readOnly />
     </div>
   );
 };
